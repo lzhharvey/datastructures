148816607c539ff33b38acdcd86277144e236cf1
username@email.com <279023678@qq.com>
2020-08-04 19:54:29 +0800

    新分支第一次提交；

--- src/main/java/com/example/datastructures/DatastructuresApplication.java
+++ src/main/java/com/example/datastructures/DatastructuresApplication.java
@@ -3,10 +3,13 @@ package com.example.datastructures;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class DatastructuresApplication {
 
     public static void main(String[] args) {
+        new ArrayList<>();
         SpringApplication.run(DatastructuresApplication.class, args);
     }
 
